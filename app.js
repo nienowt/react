1c6e42900030eb1d8cddf62bd8b43a7ec46b03ee
nienowt <elstad.nienow@gmail.com>
2016-04-23 11:03:00 -0700

    edit form toggles, does nothing

--- app.js
+++ app.js
@@ -1,42 +1,91 @@
 var postArray = [
-  {id: 1, name:"gor", bloodType:"fuck this noise"}
+  {id: 1, name:"gor", bloodType:"Bee"},
+  {id: 2, name:"swello", bloodType:"ehBee"}
 ];
 
 var Content = React.createClass({
   getInitialState: function(){
-    return {data:postArray};
+    return {data:postArray, showPostForm: true};
   },
   update: function(post){
     postArray.push(post)
     return this.setState({data:postArray})
   },
-  // polling: function(){
-  //   setInterval(this.update, 1000)
-  // },
+  showEdit:function(props){
+    console.log('hello')
+    console.log(props)
+    var singlePost = postArray.filter((post)=>{
+      return post.id == props
+    })
+    var postForm = this.state.showPostForm
+    if (this.state.data.length > 1) return this.setState({showPostForm: !postForm, data: singlePost})
+    this.setState({showPostForm: !postForm, data: postArray})
+  },
   render: function(){
     return (
       <section id="postArray">
-      <PostList data={this.state.data}></PostList>
-      <PostForm onPostSubmit = {this.update}></PostForm>
-
+      <PostList data={this.state.data} onEditClick={this.showEdit}></PostList>
+      {this.state.showPostForm ? <PostForm onPostSubmit = {this.update}></PostForm> : null}
       </section>
     );
   }
 });
 
 var EditForm = React.createClass({
+  getInitialState: function(){
+    return {name:this.props.data.name, bloodType:this.props.data.bloodType}
+  },
+  handleSubmit: function(evt){
+    evt.preventDefault()
+    console.log(this.props)
+  },
+  editName: function(evt){
+    this.setState({name: evt.target.value})
+  },
+  editBlood: function(evt){
+    this.setState({bloodType: evt.target.value})
+  },
   render: function(){
     return (
-      <button type="button">Edit</button>
+      <form className="editForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder={this.props.data.name}
+          value={this.state.name}
+          onChange={this.editName}
+        />
+        <input
+          type="text"
+          placeholder="BLOOD TYPE"
+          value={this.state.bloodType}
+          onChange={this.editBlood}
+        />
+        <button type="submit">ED~IT</button>
+      </form>
     )
   }
 })
 
 var PostList = React.createClass({
+  getInitialState: function(){
+    return {showEdit: false}
+  },
+  handleClick: function(evt){
+    console.log(this.props)
+    console.log('this is it',evt.target.id)
+    var show = this.state.showEdit
+    this.props.onEditClick(evt.target.id)
+    this.setState({showEdit:!show})
+    console.log(this.state)
+  },
   render: function(){
     var nodes = this.props.data.map((post) => {
       return (
-        <Post name = {post.name} key={post.id}>{post.bloodType}</Post>
+        <div key={post.id}>
+          <Post name = {post.name}>{post.bloodType}</Post>
+          {this.state.showEdit ? <EditForm data={post}/> : null}
+          <button id={post.id} onClick={this.handleClick} type="button">Edit</button>
+        </div>
       )
     })
     return (
@@ -53,7 +102,6 @@ var Post = React.createClass({
       <div>
         <p>{this.props.name}</p>
         <p>{this.props.children}</p>
-        <EditForm></EditForm>
       </div>
     )
   }
@@ -75,7 +123,6 @@ var PostForm = React.createClass({
     var bloodType = this.state.bloodType;
     var newPost = {id: Date.now(), name: name, bloodType: bloodType}
     this.props.onPostSubmit(newPost)
-    console.log(postArray)
     this.setState({name:'',bloodType:''})
   },
   render: function() {
